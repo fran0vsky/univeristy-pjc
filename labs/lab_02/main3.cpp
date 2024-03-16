6a6b8cf7fed928ad1c06382f24aa820e807c8e4a
fran0vsky <franekkaminski@gmail.com>
2024-03-16 23:30:25 +0100

    Initial commit

--- labs/lab_02/main3.cpp
+++ labs/lab_02/main3.cpp
@@ -24,3 +24,23 @@ auto main() -> int {
     //[x,y)
     vec2.erase(vec2.begin(), vec2.begin() + 2);
     fmt::println("{}", vec2);
+
+    auto set1 = std::set<int>(); // posortowane unikalne wartosci
+    set1.insert(2);
+    set1.insert(1);
+    set1.insert(2);
+    set1.insert(4);
+    set1.insert(3);
+    set1.insert(3);
+    fmt::println("{}", set1);
+    auto vec3 = std::vector<int>{2,4,3,1,2,3,4,5,1};
+    auto set2 = std::set(vec3.begin(),vec3.end());
+    fmt::println("{}", set2);
+    fmt::println("[{}]", fmt::join(set2, ", "));
+    vec3 = std::vector(set2.begin(), set2.end());
+    fmt::println("{}", vec3);
+
+    auto b = 7;
+    fun1(b);
+    fmt::println("{}", b);
+}
