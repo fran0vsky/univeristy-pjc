9ce772c59daa3488b4fb9ea7edf4c276c4b7bb67
fran0vsky <franekkaminski@gmail.com>
2024-03-28 23:28:37 +0100

    Initial commit

--- labs/lab_03/main5.cpp
+++ labs/lab_03/main5.cpp
@@ -7,7 +7,7 @@
 auto isPositive(int a) -> bool;
 
 auto main() -> int {
-    auto numbers = std::vector<int>{-2, 4, 6, , 1, -4, 2};
+    auto numbers = std::vector<int>{-2, 4, 6, 5, 1, -4, 2};
     auto iter = std::ranges::find(numbers,
                                   4);// find pozwala nam znalezc element o konkretnej wartości, zwraca iterator do tego elementu
     fmt::println("{}", *iter); // pobranie wartości
